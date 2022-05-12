ae5fdf942eb7151e344560697debdec2e9d231bc
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-12 18:13:02 -0400

    Update and delete in db model functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -23,21 +23,41 @@ const createGoal = asyncHandler(async (req, res) => {
         text: req.body.text
     });
 
-    res.json(goal);
+    res.status(200).json(goal);
 });
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHandler(async (req, res) => {
-    res.json({ message: `UPDATE goal ${req.params.id}` });
+    const goal = await GoalModel.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await GoalModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    });
+
+    res.status(200).json(updatedGoal);
 });
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.json({ message: `DELETE goal ${req.params.id}` });
+    const goal = await GoalModel.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove();
+
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
