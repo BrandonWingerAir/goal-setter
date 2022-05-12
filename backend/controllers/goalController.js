dc33c80aa144c284ad1a820922ef7c87942f2067
Brandon Winger-Air <brandonwinger-air@outlook.com>
2022-05-12 15:35:17 -0400

    Add express async handler for database

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'GET goals' });
-};
+});
 
 // @desc    Create goal
 // @route   POST /api/goals
 // @access  Private
-const createGoal = (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400)
         throw new Error('Please enter a goal')
     }
 
     res.json({ message: 'CREATE goal' });
-};
+});
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.json({ message: `UPDATE goal ${req.params.id}` });
-};
+});
 
 // @desc    Delete goal
 // @route   DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.json({ message: `DELETE goal ${req.params.id}` });
-};
+});
 
 module.exports = {
     getGoals,
